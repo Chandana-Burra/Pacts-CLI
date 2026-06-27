8bed67c0ad9bd6bbd7fdab0a0900a28d1fe1cfc7
Chandana Burra(UST <304525@ust.com>
2026-06-27 12:20:01 +0530

    Pacts CLI

--- src/test/java/pact/OmsProviderVerification.java
+++ src/test/java/pact/OmsProviderVerification.java
@@ -1,40 +1,3 @@
-//package pact;
-//import au.com.dius.pact.provider.junit5.HttpTestTarget;
-//import au.com.dius.pact.provider.junit5.PactVerificationContext;
-//import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
-//import au.com.dius.pact.provider.junitsupport.Provider;
-//import au.com.dius.pact.provider.junitsupport.State;
-//import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.TestTemplate;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//
-//
-//@Provider("oms-provider")
-//@PactBroker(url = "http://localhost:9292")
-//public class OmsProviderVerification {
-//
-//    @BeforeEach
-//    void setup(PactVerificationContext context){
-//        context.setTarget(new HttpTestTarget("localhost", 4010, "/"));
-//    }
-//
-//    @TestTemplate
-//    @ExtendWith(PactVerificationInvocationContextProvider.class)
-//    void verify(PactVerificationContext context){
-//        context.verifyInteraction();
-//    }
-//    @State("Order 123 exists")
-//    void isOrderExists(){
-//    }
-//    @State("Sku-9 has stock")
-//    void hasStock(){
-//    }
-//    @State("Provider can create orders")
-//    void createOrder(){
-//    }
-//
-//}
 package pact;
 
 import au.com.dius.pact.provider.junit5.HttpTestTarget;
@@ -43,21 +6,31 @@ import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvide
 import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.State;
 import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
-import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @Provider("food-provider")
-@PactBroker(url = "http://localhost:9292",
+@PactBroker(
+        url = "http://localhost:9292",
         enablePendingPacts = "true",
         providerTags = "main",
         includeWipPactsSince = "2026-06-26"
-
 )
-
-@PactFolder("target/pacts")
 public class OmsProviderVerification {
+
+    @BeforeAll
+    static void beforeAll() {
+        FoodProviderServer.startServer();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        FoodProviderServer.stopServer();
+    }
 
     @BeforeEach
     void setup(PactVerificationContext context) {
@@ -81,5 +54,4 @@ public class OmsProviderVerification {
     @State("Provider can create food orders")
     void createFoodOrder() {
     }
-
 }
